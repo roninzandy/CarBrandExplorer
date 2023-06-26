a2a443a9a796ec1347dc93b9ee14dcad11807069
roninzandy <suddenlyandy@gmail.com>
2023-06-26 18:57:53 +0600

    Update urls.py

--- coolsite/urls.py
+++ coolsite/urls.py
@@ -19,7 +19,7 @@ from django.contrib import admin
 from coolsite import settings
 from women.views import *
 from django.urls import path, include
-
+#hey
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('captcha/', include('captcha.urls')),
